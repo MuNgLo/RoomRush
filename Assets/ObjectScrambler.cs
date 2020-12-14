c99769512020ae7e75deb1099eea54499ed2c907
Zas32x <zas.email@gmail.com>
2020-12-14 03:37:22 +0200

    object scrambler

---
+++ Assets/ObjectScrambler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ObjectScrambler: MonoBehaviour {
+	public Transform[] scrambledObjects;
+
+	public void Start() {
+		scramble();
+	}
+
+	public void scramble() {
+		List<Transform> deck = scrambledObjects.ToList();
+		List<Location> result = new List<Location>(scrambledObjects.Length);
+		//shuffle
+		while (deck.Count > 0) {
+			int index = Random.Range(0, deck.Count);
+			result.Add(new Location(deck[index]));
+			deck.RemoveAt(index);
+		}
+		//deal
+		for (int i = 0; i < scrambledObjects.Length; i++) {
+			scrambledObjects[i].SetPositionAndRotation(result[i].position, result[i].rotation);
+		}
+	}
+
+	private struct Location {
+		public Vector3 position;
+		public Quaternion rotation;
+
+		public Location(Transform t) {
+			position = t.position;
+			rotation = t.rotation;
+		}
+	}
+}
